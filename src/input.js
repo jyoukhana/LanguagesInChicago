207a5c7af4d189021078a9d09f20b705c51521dc
Genevieve Myers <genevievemmyers@gmail.com>
2020-06-05 13:44:03 -0500

    moved select to dashborad and make some minor tweaks to chart pages

--- src/input.js
+++ src/input.js
@@ -2,29 +2,42 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import 'chartjs-plugin-labels'
-import { NativeSelect } from '@material-ui/core';
-import { useHistory } from "react-router-dom";
-import GetData from './getData.js';
+import { CardActionArea, Typography } from '@material-ui/core';
 
+import GetData from './getData.js';
+import GetImage from './getImage.js';
 
 class InputChart extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             chart: undefined,
-            community: '',
+            community: undefined,
             isLoaded: false,
-
+            population: undefined
         }
     }
-
-    componentDidMount() {
-        if (this.state.chart != undefined) this.state.chart.destroy();
+    componentDidUpdate(prevProps, prevState) {
         var query = new URLSearchParams(window.location.search);
-        console.log('componet mounted' + query.values(0));
         var name = query.get('name');
+        if (name !== this.state.community) {
+            this.upDateData(name);
+        }
 
+    }
+
+    toUpper(str) {
+        var splitStr = str.toLowerCase().split('_');
+        for (var i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        return splitStr.join(' ');
+    }
+
+    upDateData(name) {
+        if (this.state.chart !== undefined) this.state.chart.destroy();
         GetData(name)
             .then(
                 (result) => {
@@ -38,14 +51,16 @@ class InputChart extends React.Component {
                         var b = Math.floor(Math.random() * 255);
                         return "rgb(" + r + "," + g + "," + b + ")";
                     };
-
+                    var totalIndividuals = 0;
                     for (let i = 2; i < keys.length; i++) {
                         if (values[i] > 0) {
                             dataset.push(values[i]);
-                            label.push(keys[i]);
+                            label.push(this.toUpper(keys[i]));
                             coloR.push(dynamicColors());
+                            totalIndividuals += parseInt(values[i]);
                         }
                     }
+
                     var Chart = require('chart.js');
                     var ctx = document.getElementById('myChart').getContext('2d');
                     const myPieChart = new Chart(ctx, {
@@ -58,77 +73,76 @@ class InputChart extends React.Component {
                             }],
                             labels: label
                         },
-                        options: [{
-                            plugins: {
-                                labels: {
-                                    render: 'percentage',
-                                    position: 'border',
-                                    overlap: false,
-                                }
-                            }
-                        }]
+                        options: {
+                            tooltips: {
+                                mode: 'index',
+                                callbacks: {
+                                    footer: function (tooltipItems, data) {
+                                        var percentage = 0;
+
+                                        tooltipItems.forEach(function (tooltipItem) {
+                                            percentage = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index] / totalIndividuals * 100;
+                                        });
+
+                                        return 'Percentage: ' + percentage.toFixed(1) + '%';
+                                    }
+                                },
+                                footerFontStyle: 'normal'
+                            },
+                        }
                     });
                     this.setState({
                         chart: myPieChart,
                         community: name,
                         isLoaded: true,
+                        population: totalIndividuals
                     })
                 })
             .catch(error => {
                 console.error("Request failed", error);
             })
+    }
 
+    componentDidMount() {
+        var query = new URLSearchParams(window.location.search);
+        var name = query.get('name');
+        this.upDateData(name);
     }
 
     render() {
-        let community = this.state.data;
-        let sub = "Language distrabutions for " + this.state.community;
-        return (
-            <Card>
-                <CardHeader
-                    title="Community language visualizer"
-                    subheader={sub}
-                />
-                <CardContent>
-                    <canvas id="myChart" width="300" height="300"></canvas>
-                </CardContent>
-            </Card>
-        )
+        var query = new URLSearchParams(window.location.search);
+
+        if (query.get("name") != null) {
+            let sub = "Language distributions for " + this.state.community + " (population " + this.state.population + ").";
+            let neighborhoodImage = GetImage(this.state.community);
+            return (
+                <Card>
+                    <CardActionArea>
+                        <CardHeader
+                            title="Community language visualizer"
+                            subheader={sub}
+                        />
+                        <CardMedia
+                            component="img"
+                            alt={"image that shows the " + this.state.community + " community"}
+                            height="400"
+                            image={neighborhoodImage}
+                            title={this.state.community}
+                        />
+                    </CardActionArea>
+                    <CardContent>
+                        <canvas id="myChart" width="300" height="300"></canvas>
+                    </CardContent>
+                </Card>
+            )
+        } else {
+            return (
+                <Typography>
+                    Please select a community from the selector to visualize language data.
+                </Typography>
+            )
+        }
     }
-
 }
+
 export default InputChart;
-/*<FormControl >
-                                <InputLabel id="select1">Community</InputLabel>
-                                <Select name="name"
-                                    labelId="select1"
-                                    id="demo-simple-select"
-                                    value={this.state.community}
-                                    onChange={(event) => this.handleChange(event.target.value)}
-                                >
-                                    {community.map((item, i) => {
-                                        return <MenuItem value={i}>{item.community_area_name}</MenuItem>
-                                    })}
-                                </Select>
-                            </FormControl>
-
-
-
-                            <select onChange={(event) => this.handleChange(event.target.value)} name="mane" id="communities">
-                                {community.map((item, i) => {
-                                    return <option value={i}>{item.community_area_name}</option>
-                                })}
-                            </select>
-
-
-
-                            <form onSubmit={(event) => this.handleSubmit(event.target.value)}>
-                                    <input list="selector" name="name" />
-                                    <datalist id="selector" name="mane">
-                                        {community.map((item, i) => {
-                                            return <option data-value={i} >{item.community_area_name}</option>
-                                        })}
-                                    </datalist>
-                                    <input type="submit" />
-                                </form>
-                            */
--- src/routingPage.js
+++ src/routingPage.js
@@ -7,11 +7,33 @@ import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
+import HomeIcon from '@material-ui/icons/Home';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+
+import { blue } from '@material-ui/core/colors';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   Link,
+  withRouter,
+  useHistory,
+  useLocation
 } from "react-router-dom";
 import ChartPage from './chartPage.js';
 import GetData from './getData.js';
@@ -38,7 +60,7 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'auto',
   },
   listText: {
-    fontSize: 16
+    fontSize: 14
   },
   content: {
     flexGrow: 1,
@@ -47,16 +69,40 @@ const useStyles = makeStyles((theme) => ({
   message: {
     color: "black"
   },
+  root2: {
+    maxWidth: 1300,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: blue[500],
+  },
 }));
 
 //Note to self: use uppercase for name of export default function
 //otherwise it compilier complains about hook
-export default function RoutePage() {
+function RoutePage() {
   const classes = useStyles();
   const [languageData, setLanguageData] = useState("");
   const [communities, setComData] = useState("");
+  const [data, setData] = useState('')
+  const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
+
     GetData()
       .then((data) => {
         let count = 0;
@@ -71,20 +117,23 @@ export default function RoutePage() {
           //When I type j in data[i], j is the name of the key
           //so to get to the value, I have to type data[i][j]
           for (let j in data[i]) {
-            if (j != "community_area" && j != "community_area_name" && j != "predominant_non_english_language_") {
+            if (j !== "community_area" && j !== "community_area_name" && j !== "predominant_non_english_language_") {
               total[count] += parseInt(data[i][j]);
               count++;
             }
           }
-
           count = 0;
         }
 
         // I created this instead of using Object.keys(data[0]) 
         // because I don't want the keys specified following if statement.
         for (let key in data[0]) {
-          if (key != "community_area" && key != "community_area_name" && key != "predominant_non_english_language_") {
-            allLanguages[count] = key.replace(/_/g, " "); //the first argument /_/g means to replace all underscores in the String 
+          if (key !== "community_area" && key !== "community_area_name" && key !== "predominant_non_english_language_") {
+            var splitStr = key.toLowerCase().split('_');
+            for (var i = 0; i < splitStr.length; i++) {
+              splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+            }
+            allLanguages[count] = splitStr.join(' ');; //the first argument /_/g means to replace all underscores in the String 
             count++;
           }
         }
@@ -104,19 +153,23 @@ export default function RoutePage() {
           language: allLanguages,
           color: colors
         });
-        setComData(comOptions)
+        setComData(comOptions.sort())
       });
-  }, []);
+  }
+    , []);
+
+
 
 
   //Composition of website...Not the official version, Just a placeholder until the official version is finished. 
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" noWrap>
-            Final Project Concept
+            Chicago Language Data
           </Typography>
         </Toolbar>
       </AppBar>
@@ -129,25 +182,54 @@ export default function RoutePage() {
       >
         <Toolbar />
         <div className={classes.drawerContainer}>
-          <div>
-            <List className={classes.listText}>
-              <Link to="/">
-                <ListItem button>Home</ListItem>
-              </Link>
-              <Link to="/chart">
-                <ListItem button>Chart</ListItem>
-              </Link>
-              <Link to="/community">
-                <ListItem button>Data by Community</ListItem>
-              </Link>
-            </List>
-          </div>
+
+          <List className={classes.listText}>
+            <Link to="/">
+              <ListItem button><HomeIcon />Home</ListItem>
+            </Link>
+            <Link to="/chart">
+              <ListItem button><AssessmentIcon />City-Wide Language Visualizer</ListItem>
+            </Link>
+
+            <ListItem>
+              <Typography >
+                {(Object.entries(communities).length > 0) ? (
+
+                  <FormControl variant="outlined" >
+                    <InputLabel htmlFor="Community selector">Community</InputLabel>
+                    <NativeSelect
+                      value={(location.pathname === "/community") ? (data) : ""}
+                      onChange={(event) => {
+                        setData(event.target.value)
+                        history.push({
+                          pathname: '/community',
+                          search: '?name=' + encodeURIComponent(event.target.value)
+                        })
+
+                      }}
+                      inputProps={{
+                        name: 'age',
+                        id: 'Community selector',
+                      }}
+                    >
+                      <option aria-label="None" disabled></option>
+                      {communities.map((item, i) => {
+                        return <option aria-label={item} value={item}>{item}</option>
+                      })}
+                    </NativeSelect>
+                    <FormHelperText>Select a community to view language distributions</FormHelperText>
+                  </FormControl>) : ((<p></p>))}
+              </Typography>
+            </ListItem>
+          </List>
+
         </div>
       </Drawer>
 
       <main className={classes.content}>
         <Toolbar />
         <Switch>
+
           <Route path="/chart">
             <ChartPage languageData={languageData} />
           </Route>
@@ -155,7 +237,7 @@ export default function RoutePage() {
             <CommunityPage />
           </Route>
           <Route path="/">
-            <Home communities={communities} />
+            <Home />
           </Route>
         </Switch>
       </main>
@@ -163,32 +245,63 @@ export default function RoutePage() {
   );
 }
 
+
+
 //When the website initially opens, the user should be brought to the Home page
 function Home(props) {
   const classes = useStyles();
-  const communities = props.communities;
-  console.log(communities);
+  const [expanded, setExpanded] = React.useState(false);
 
-
-  //below is the basic from I had t take input for chart onSubmit,
-  //              //communities.map((item, i) => {
-  //return <option value={item.community_area_name} >{item.community_area_name}</option>})
   return (
-    <Typography className={classes.message}>
-      Hello, this is the dummy Home page
-{(Object.entries(communities).length > 0) ? (
-        <form >
-          <input list="selector" name="name" />
-          <datalist id="selector" name="mane">
-            {props.communities.map((item, i) => {
-              return <option value={item} />
-            })}
-          </datalist>
-          <input type="submit" />
-        </form>
-      ) : ((<p></p>))}
+    <Card className={classes.root2}>
+      <CardHeader
+        avatar={
+          <Avatar alt="Chicago Flag" src="/images/chicago-flag.png" aria-label="chicago flag icon" />
+        }
+        title="Chicago Illinois"
+        subheader="One of the most diverse cities in the US"
+      />
+      <CardMedia
+        className={classes.media}
+        image="/images/city-pan.jpg"
+        title="Chicago, Illinois"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This application displays language data within Chicago and its many neighborhoods.
+          Click the collapse arrow below for more info.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
 
-    </Typography>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={() => {
+            setExpanded(!expanded)
+          }}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Overview:</Typography>
+          <Typography paragraph>
+            This project displays census data of non-English languages spoken in Chicago from 2008 - 2012. The data is pulled
+            from the Chicago Data Portal and uses their API to fetch JSON data of each neighborhood and their language
+            statistics. In this application, you can view a bar chart of city-wide language statistics by clicking the
+            link in the drawer on the left. Alternatlivley, you may select a community to
+            view a pie chart of any given neighborhood's language statistics.
+            All data is taken from https://data.cityofchicago.org/Health-Human-Services/Census-Data-Languages-spoken-in-Chicago-2008-2012/a2fk-ec6q
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
 }
+export default withRouter(RoutePage);
 
